50f9279af0c5059d48083c74a705aea5d49b6e2a
Karan <skkaranswami@gmail.com>
2023-09-28 20:27:33 -0400

    printTableFn added

--- main.go
+++ main.go
@@ -12,7 +12,16 @@ func swap(a, b int) (int, int) {
 	return b, a
 }
 
+// Karan function 500224005
+func printTableOf15() {
+	for i := 1; i <= 10; i++ {
+		result := 15 * i
+		log.Printf("15 x %d = %d\n", i, result)
+	}
+}
+
 func main() {
 	log.Println(add(42, 13))
 	log.Println(swap(5, 7))
+	log.Println("Multiplication Table of 15:")
 }
